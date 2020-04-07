fbffe8efaa30175000f02cc0b9e0f02459a4f32f
CiaraMaria Proia <proiacm@gmail.com>
2020-04-07 18:31:23 +0000

    Automatically backed up by Learn

---
+++ lib/project.rb
@@ -0,0 +1,15 @@
+class Project
+  attr_accessor :backers
+  attr_reader :title 
+  
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+  
+  def add_backer(backer)
+    @backers << backer
+    backer.back_project(self) if !backer.backed_project
+  end
+    
+end
